b18e198575f518cf85bfa744f4e24413ff33b763
Austin Schaffer <schaffer.austin.t@gmail.com>
2021-05-13 17:01:32 -0400

    Added persisting data to couchdb

--- notebook.py
+++ notebook.py
@@ -7,7 +7,7 @@ print("Loading package names...")
 
 simple_index_resp = requests.get("https://pypi.org/simple")
 soup = bs4.BeautifulSoup(simple_index_resp.text, "html.parser")
-package_names = [ link.text for link in soup.find_all('a') ]
+package_names = ( link.text for link in soup.find_all('a') )
 
 # %% Get Versions for Each Package
 
@@ -17,6 +17,7 @@ from typing import Optional, Iterator
 
 import aiohttp
 from aiostream import stream, pipe, async_
+import aiocouch
 
 import pydeps
 
@@ -35,17 +36,27 @@ async def get_package_info(session: aiohttp.ClientSession, package_name: str) ->
     except Exception as e:
         return e, PackageInfo(package_name, {})
 
+async def insert_record(db: aiocouch.database.Database, record: PackageInfo):
+    doc = await db.create(
+        record.name,
+        exists_ok=True,
+        data=record.info
+    )
+
+    return await doc.save()
+
 async def main():
     async with aiohttp.ClientSession() as session:
-        pipeline = (
-            stream.iterate(package_names)
-            | pipe.map(async_(lambda name: get_package_info(session, name)))
-            | pipe.filter(lambda result: not result[0])
-            | pipe.map(lambda result: result[1])
-            | pipe.print("Data: {}")
-        )
+        async with aiocouch.CouchDB("http://localhost:5984", user="pydeps", password="pydeps") as couchdb:
+            db = await couchdb.create("db", exists_ok=True)
 
-        await pipeline
+            await (
+                stream.iterate(package_names)
+                | pipe.map(async_(lambda name: get_package_info(session, name)))
+                | pipe.filter(lambda result: not result[0])
+                | pipe.map(lambda result: result[1])
+                | pipe.map(async_(lambda record: insert_record(db, record)))
+            )
 
 print("Fetching package info...")
 asyncio.run(main())
