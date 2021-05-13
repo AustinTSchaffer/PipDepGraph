7f37f05245510d3ddf8d418df47f695b072b4aaf
Austin Schaffer <schaffer.austin.t@gmail.com>
2021-05-12 20:37:27 -0400

    Started working on async notebook

---
+++ notebook.py
@@ -0,0 +1,50 @@
+#%% Get List of Packages
+
+import requests
+import bs4
+
+print("Loading package names...")
+
+simple_index_resp = requests.get("https://pypi.org/simple")
+soup = bs4.BeautifulSoup(simple_index_resp.text, "html.parser")
+package_names = [ link.text for link in soup.find_all('a') ]
+
+# %% Get Versions for Each Package
+
+import pydeps
+import asyncio
+import aiohttp
+from aiostream import stream, pipe, async_
+import contextlib
+import dataclasses
+from typing import Optional, Iterator
+
+@dataclasses.dataclass
+class PackageInfo:
+    name: str
+    info: dict
+
+async def get_package_info(session: aiohttp.ClientSession, package_name: str) -> tuple[Optional[Exception], PackageInfo]:
+    try:
+        package_info = await pydeps.get_package_info_async(
+            session=session,
+            package=package_name,
+        )
+        return None, PackageInfo(package_name, package_info)
+    except Exception as e:
+        return e, PackageInfo(package_name, {})
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        pipeline = (
+            stream.iterate(package_names)
+            | pipe.map(async_(lambda name: get_package_info(session, name)))
+            | pipe.filter(lambda result: not result[0])
+            | pipe.map(lambda result: result[1])
+            | pipe.print("Data: {}")
+        )
+
+        await pipeline
+
+print("Fetching package info...")
+asyncio.run(main())
--- pydeps.py
+++ pydeps.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python
 
+import asyncio
 import argparse
 import json
 from typing import Optional
+import logging
+import sys
 
+import aiohttp
 import requests
 import jsonpath_ng
-
-parser = argparse.ArgumentParser(description="Pulls information about packages from PyPI via the /pypi/{package}/json endpoint")
-parser.add_argument(type=str, help="The name of a python package.", dest="package")
-parser.add_argument("--version", "-v", type=str, help="The optional version of a python package.", dest="version")
-parser.add_argument("--json-path", "--jp", "-p", type=str, help="A JSONPath expression for limiting the output.", dest="jsonpath")
-
 PYPI_ENDPOINT = "https://pypi.org/pypi"
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
+    stream=sys.stderr,
+)
+logger = logging.getLogger(__name__)
+
+async def get_package_info_async(session: aiohttp.ClientSession, package: str, version: Optional[str]=None) -> dict:
+    """
+    """
+
+    uri = (
+        f'{PYPI_ENDPOINT}/{package}/{version}/json'
+        if version else
+        f'{PYPI_ENDPOINT}/{package}/json'
+    )
+
+    async with session.get(uri) as response:
+        return await response.json()
+
 
 def get_package_info(package: str, version: Optional[str]=None, jsonpath: Optional[str]=None) -> dict:
     """
@@ -43,6 +63,11 @@ def get_package_info(package: str, version: Optional[str]=None, jsonpath: Option
     raise ValueError(f"Error from PyPI. Status code: {resp.status_code}")
 
 def main():
+    parser = argparse.ArgumentParser(description="Pulls information about packages from PyPI via the /pypi/{package}/json endpoint")
+    parser.add_argument(type=str, help="The name of a python package.", dest="package")
+    parser.add_argument("--version", "-v", type=str, help="The optional version of a python package.", dest="version")
+    parser.add_argument("--json-path", "--jp", "-p", type=str, help="A JSONPath expression for limiting the output.", dest="jsonpath")
+
     args = parser.parse_args()
     try:
         info = get_package_info(args.package, args.version, args.jsonpath)
